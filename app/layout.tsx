004cbd80ffbd8951289aa88578bdfc31e487e01a
NeftaliAguilar <hola@neftaliaguilar.com>
2026-09-03 13:58:42 -0600

    feat: new blog post added + analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "@neftaliaguilar/ui/styles.css";
 import "./globals.css";
 
@@ -70,7 +71,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeSyncScript }} />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
